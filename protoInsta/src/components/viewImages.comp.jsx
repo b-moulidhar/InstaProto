a969e6895949c51e2f67f42cccb7905ef9472eef
Moulidhar B <moulidhar.b@valtech.com>
2023-07-20 14:20:06 +0530

    getting data from database

--- protoInsta/src/components/viewImages.comp.jsx
+++ protoInsta/src/components/viewImages.comp.jsx
@@ -7,46 +7,50 @@ const ViewComp = () => {
   
   const refresh = () => {
     axios.get("http://localhost:5000/getImages").then((res) => {
-        console.log(res.data)
+      console.log(res.data);
       setHeroes(res.data);
       setLoading(false);
     });
   };
+
   useEffect(() => {
     refresh();
   }, []);
 
-
   // Function to convert base64 to blob and create URL
-  const createBlobUrl = async(base64String, fileType) => {
+  const createBlobUrl = (base64String, fileType) => {
     const byteCharacters = atob(base64String);
     const byteNumbers = new Array(byteCharacters.length);
     for (let i = 0; i < byteCharacters.length; i++) {
       byteNumbers[i] = byteCharacters.charCodeAt(i);
     }
     const byteArray = new Uint8Array(byteNumbers);
-    console.log(byteArray);
-  
+
     // Set the correct MIME type based on the fileType parameter
     let mimeType;
     switch (fileType) {
-      case "jpeg":
+      case "image/jpeg":
         mimeType = "image/jpeg";
         break;
-      case "pdf":
+      case "application/pdf":
         mimeType = "application/pdf";
         break;
-      case "png":
+      case "image/png":
         mimeType = "image/png";
         break;
       default:
         mimeType = "image/png"; // Default to image/jpeg if the fileType is unknown
         break;
     }
-    console.log(URL.createObjectURL(new Blob([byteArray], { type: mimeType })));
-    return URL.createObjectURL(new Blob([byteArray], { type: mimeType }));
+
+    const blob = new Blob([byteArray], { type: mimeType });
+    return URL.createObjectURL(blob);
   };
-  
+
+  // Function to revoke the URL and free up memory
+  const revokeBlobUrl = (url) => {
+    URL.revokeObjectURL(url);
+  };
 
   return (
     <>
@@ -60,10 +64,13 @@ const ViewComp = () => {
             <p>{hero.filetype}</p>
             {/* Render the image using createBlobUrl */}
             <img src={createBlobUrl(hero.filedata, hero.filetype)} alt={`Hero ${index}`} />
-            {
-              hero.filetype === "application/pdf" &&
-            <a href={createBlobUrl(hero.filedata, hero.filetype)} download={hero.filename}>download</a>
-            }
+            {hero.filetype === "application/pdf" && (
+              <a href={createBlobUrl(hero.filedata, hero.filetype)} download={hero.filename}>
+                download
+              </a>
+            )}
+            {/* Don't forget to revoke the URL after the download link is clicked */}
+            {hero.filetype === "application/pdf" && revokeBlobUrl(createBlobUrl(hero.filedata, hero.filetype))}
           </div>
         ))
       )}
